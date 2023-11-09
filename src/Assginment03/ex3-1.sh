6f97a3543831f6c4fe1f856f4899391f639a3f81
janghoonki <129042085+janghoonki@users.noreply.github.com>
2023-11-09 15:27:01 +0900

    Update ex3-1.sh

--- src/Assginment03/ex3-1.sh
+++ src/Assginment03/ex3-1.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+
+if [ -n "$1" ] 
+then
+    c=$1
+    while [ $c -ne 0 ]
+    do
+        echo "hello world"
+        c=$(($c - 1))
+    done
+else
+    while [ 1 ]
+    do
+        echo "Hello World"
+    done
+fi
+
+exit 0
 
